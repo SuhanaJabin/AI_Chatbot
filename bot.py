34adce8e23dbdf49748e6afe612035f0084103c6
SuhanaJabin <suhanajabinpk@gmail.com>
2023-06-13 22:46:19 +0530

    CLI chatbot created

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Write an essay on flowers"}
+        {"role": "user", "content": "What is meant by click fraud zombie?"}
     ]
 )
 
