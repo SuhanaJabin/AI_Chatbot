27965b33ba39b05893e5cc8139445c775ca8d6d0
SuhanaJabin <suhanajabinpk@gmail.com>
2023-06-13 22:25:11 +0530

    simple chatbot completed

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
-print("hello world")
+import openai
+
+openai.api_key = "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"
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Write an essay on flowers"}
+    ]
+)
+
+message = response['choices'][0]['message']['content']
+print(message)
